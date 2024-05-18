1449bfe20463ff33ffa1d515d3c75bee6ef8c8fe
ahuoguo <ahuoguo@gmail.com>
2024-05-18 16:51:03 -0400

    test mk

---
+++ tests/cvc5-test.scm
@@ -0,0 +1,7 @@
+;; you should call this file in the toplevel directory of this repo
+
+(load "mk.scm")
+(load "smt.scm")
+(load "cvc5-driver.scm")
+(load "test-check.scm")
+(load "talk.scm")
---
+++ tests/z3-test.scm
@@ -0,0 +1,7 @@
+;; you should call this file in the toplevel directory of this repo
+
+(load "mk.scm")
+(load "smt.scm")
+(load "z3-driver.scm")
+(load "test-check.scm")
+(load "talk.scm")
